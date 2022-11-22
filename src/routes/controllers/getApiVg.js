9e02aa59863d3e727e693d7760cfe327846b7aa8
NadiaO089 <nadiaolivera@gmail.com>
2022-11-22 20:30:24 -0300

    update

--- src/routes/controllers/getApiVg.js
+++ src/routes/controllers/getApiVg.js
@@ -7,33 +7,50 @@ const getApiVideogames = async (name) => {
     var URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
     var data = [];
 
-    while(URL !== `https://api.rawg.io/api/games?key=${API_KEY}&page=99`){
-        
-        allVideogames = await axios.get(URL);
+    if (name) {
+        while(data.length < 10000){
             
-        allVideogames.data.results.map(r => {
-            data.push({
-                id: r.id,
-                name: r.name,
-                background_image: r.background_image,
-                rating: r.rating,
-                genres: r.genres.map(g => {
-                    return {name: g.name, id: g.id}
-                })
-            });
-        })
-
-        URL = allVideogames.data.next;
-}
-
-if (name) {
-    let byName = data.filter(e => e.name.toLowerCase().includes(name.toLocaleLowerCase()))
-    if(byName.length > 15) byName = byName.slice(0,14);
-        return byName;
-} else {
-    data = data.slice(0,99);
-    return data
-}
+            allVideogames = await axios.get(URL);
+                
+            allVideogames.data.results.map(r => {
+                data.push({
+                    id: r.id,
+                    name: r.name,
+                    background_image: r.background_image,
+                    rating: r.rating,
+                    genres: r.genres.map(g => {
+                        return {name: g.name, id: g.id}
+                    })
+                });
+            })
+    
+            URL = allVideogames.data.next;
+    }
+        let byName = data.filter(e => e.name.toLowerCase().includes(name.toLocaleLowerCase()))
+        if(byName.length > 15) byName = byName.slice(0,14);
+            return byName;
+            
+    } else {
+        while(data.length < 100){
+            
+            allVideogames = await axios.get(URL);
+                
+            allVideogames.data.results.map(r => {
+                data.push({
+                    id: r.id,
+                    name: r.name,
+                    background_image: r.background_image,
+                    rating: r.rating,
+                    genres: r.genres.map(g => {
+                        return {name: g.name, id: g.id}
+                    })
+                });
+            })
+    
+            URL = allVideogames.data.next;
+    }
+        return data
+    }
     
 
 }
